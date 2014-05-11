d079f543e58b378be842c32019248a079cc9efc3
Ian Noyes <ian@12southmusic.com>
2014-05-11 17:55:38 -0500

    try this

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -6,14 +6,7 @@
 </div>
 <div class="row" id="bg-2">
 	<div class="col-sm-6">
-		<? php
-			$the_query = new WP_Query( 'page_id=44' )
-			while ( $the_query->have_posts() ) :
-				$the_query->the_post();
-			        the_content();
-			endwhile;
-			wp_reset_postdata();
-		?>
+		
 	</div>
 	<div class="col-sm-6">
 		<img src="...." id="bg-2-img" />

50d110eced63af6531f5f8a0f253dba633f30936
Ian Noyes <ian@12southmusic.com>
2014-05-11 17:38:45 -0500

    home template

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -1,4 +1,25 @@
-<?php while (have_posts()) : the_post(); ?>
-  <?php the_content(); ?>
-  <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
-<?php endwhile; ?>
+<div class="row" id="bg-1">
+	<div class="col-sm-12">
+	<?php echo do_shortcode('[wpv-view name="Home page - Slider"]'); ?>
+	</div>
+</div>
+<div class="row" id="bg-2">
+	<div class="col-sm-6">
+		<? php
+			$the_query = new WP_Query( 'page_id=44' )
+			while ( $the_query->have_posts() ) :
+				$the_query->the_post();
+			        the_content();
+			endwhile;
+			wp_reset_postdata();
+		?>
+	</div>
+	<div class="col-sm-6">
+		<img src="...." id="bg-2-img" />
+	</div>
+</div>
+<div class="row" id="bg-3">
+	<div class="col-sm-8">
+	<?php echo do_shortcode('[wpv-view name="homecrossfaderthing"]'); ?>
+	</div>
+</div>

c356093c13046c01f50410a02b67dde3fffa877a
Ian Noyes <ian@12southmusic.com>
2014-05-12 14:53:04 -0500

    home slider styling

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -1,6 +1,5 @@
 <div class="row" id="bg-1">
-	<div class="col-sm-12">
-	<h1>TEST</h1>
+	<div class="col-sm-12 home-slider-container">
 	<?php echo do_shortcode('[wpv-view name="home-top-banners"]'); ?>
 	</div>
 </div>

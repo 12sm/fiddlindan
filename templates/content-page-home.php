4d9f9aa43b2de5affc7dd29b693cb114ebf73458
Ian Noyes <ian@12southmusic.com>
2014-05-12 15:34:09 -0500

    #home-2-content

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -5,12 +5,14 @@
 </div>
 <div class="row" id="bg-2">
 	<div class="col-sm-6">
+		<div id="home-2-content">
 		<?php
 		$page_id = 44;  //Page ID
 		$page_data = get_page( $page_id );
 		$content = apply_filters('the_content', $page_data->post_content);
 		echo $content;
 		?>
+		</div>
 	</div>
 </div>
 <div class="row" id="bg-3">

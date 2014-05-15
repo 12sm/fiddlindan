fc533bcc612bfc98afd6d564dde3d8a1495171cf
Ian Noyes <ian@12southmusic.com>
2014-05-15 12:50:27 -0500

    photo page template

---
+++ templates/content-page-photos.php
@@ -0,0 +1,11 @@
+<div class="row">
+	<div class="col-sm-12">
+	<?php the_content(); ?>
+	</div>
+</div>
+<div class="row">
+	<div class="col-sm-12">
+			<ul class="photos-page">
+			</ul>
+	</div>
+</div>

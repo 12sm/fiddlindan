6767c7832116f6a7c44427bc4e88ed0d273302ea
Ian Noyes <ian@12southmusic.com>
2014-05-16 10:38:09 -0500

    add preloadjs CDN

--- templates/head.php
+++ templates/head.php
@@ -24,7 +24,7 @@ http://12southmusic.com/
   <?php wp_head(); ?>
    <link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/smoothness/jquery-ui.css" />
 <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
-<script src="http://code.createjs.com/soundjs-0.5.2.min.js"> </script>
+<script src="http://code.createjs.com/soundjs-0.5.2.min.js"></script>
   <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo home_url(); ?>/feed/">
   <link rel="shortcut icon" href="<?php echo of_get_option('favicon_upload'); ?>" />
 </head>

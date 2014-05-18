c749c5d647269fb3b25189aacadeb01de0d2d4a4
Ian Noyes <ian@12southmusic.com>
2014-05-18 15:44:48 -0500

    more debugging

--- templates/content-page-sessions.php
+++ templates/content-page-sessions.php
@@ -5,11 +5,11 @@
 <?php endwhile; ?>
 	</div>
 	<div class="col-sm-8">
-<?php echo do_shortcode('[wpv-view name="Audio Slider 2.0"]'); ?>
+<!--<?php echo do_shortcode('[wpv-view name="Audio Slider 2.0"]'); ?>-->
 	</div>
 </div>
-<div class="row" id="bg-4"> 
-	<!--<div class="col-sm-12">
+<!--<div class="row" id="bg-4"> 
+	<div class="col-sm-12">
 		<?php echo do_shortcode('[vfb id=1]'); ?>
 	</div>
 </dvi>-->

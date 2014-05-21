7e240edb7558dddeb41f253a8fafa9f23b7ca766
Ian Noyes <ian@12southmusic.com>
2014-05-21 11:04:36 -0500

    padding in header

--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -8,13 +8,16 @@
         <span class="icon-bar"></span>
       </button>
     </div>
-
-    <nav class="collapse navbar-collapse" role="navigation">
-      <?php
-        if (has_nav_menu('primary_navigation')) :
-          wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav'));
-        endif;
-      ?>
-    </nav>
+  <div class="row">
+    <div class="col-sm-11 col-sm-offset-1">
+      <nav class="collapse navbar-collapse" role="navigation">
+        <?php
+          if (has_nav_menu('primary_navigation')) :
+            wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav'));
+          endif;
+        ?>
+        </nav>
+      </div>
+    </div>
   </div>
 </header>

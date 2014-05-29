1376d7ca303c369ba27a76f4a435c43081233236
Sarah <Sarah@12southmusic.com>
2014-05-29 12:44:54 -0500

    changing col numbers

--- templates/content-page-sessions.php
+++ templates/content-page-sessions.php
@@ -1,10 +1,10 @@
 <div class="row col">
-	<div class="col-sm-5">
+	<div class="col-sm-4">
 <?php while (have_posts()) : the_post(); ?>
   <?php the_content(); ?>
 <?php endwhile; ?>
 	</div>
-	<div class="col-sm-7">
+	<div class="col-sm-8">
 <?php echo do_shortcode('[wpv-view name="Audio Slider 2.0"]'); ?>
 	</div>
 </div>

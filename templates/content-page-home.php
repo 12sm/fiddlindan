84b64e075579ef9dc34c5e44855eec058e830e6d
Sarah <Sarah@12southmusic.com>
2014-05-30 18:03:27 -0500

    tweakin

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -21,7 +21,7 @@
 	</div>
 </div>
 <div class="row home-content" id="bg-3">
-	<div class="col-xs-12 col-sm-12 col-md-8 col-lg-8 col-sm-offset-1">
+	<div class="col-xs-12 col-sm-12 col-md-8 col-lg-8">
 		<div id="home-3-content">
 					<h2>Dan Can<br />
 					Rock Your Track</h2>

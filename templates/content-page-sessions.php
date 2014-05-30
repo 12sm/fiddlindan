05f8819f8cc3719409c2787670e95b21aae253d6
Sarah <Sarah@12southmusic.com>
2014-05-30 15:07:42 -0500

    med layout sessions

--- templates/content-page-sessions.php
+++ templates/content-page-sessions.php
@@ -1,11 +1,11 @@
 <div class="row dan-bg">
 	<div class="col">
-		<div class="col-sm-4">
+		<div class="col-sm-4 col-md-12">
 	<?php while (have_posts()) : the_post(); ?>
 	  <?php the_content(); ?>
 	<?php endwhile; ?>
 		</div>
-		<div class="col-sm-8">
+		<div class="col-sm-8 col-md-12">
 	<?php echo do_shortcode('[wpv-view name="Audio Slider 2.0"]'); ?>
 		</div>
 	</div>

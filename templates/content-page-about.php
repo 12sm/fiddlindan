5b74aeb44d696696a5eb8ff6e183fe6cf555ad42
Sarah <Sarah@12southmusic.com>
2014-05-30 18:24:38 -0500

    about page changes for tabbies

--- templates/content-page-about.php
+++ templates/content-page-about.php
@@ -1,5 +1,5 @@
 <div class="row about-row">
-	<div class="col-sm-6 col-xs-12">
+	<div class="col-sm-12 col-xs-12">
 <?php while (have_posts()) : the_post(); ?>
   <?php the_content(); ?>
 <?php endwhile; ?>

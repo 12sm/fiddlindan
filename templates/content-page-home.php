c51f2df48c04b1d11e4a50fadd7b2a60b9cdbcfb
Sarah <Sarah@12southmusic.com>
2014-06-25 11:37:07 -0500

    commenting out bio section on home page

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -3,23 +3,23 @@
 	<?php echo do_shortcode('[wpv-view name="home-top-banners"]'); ?>
 	</div>
 </div>
-<div class="row home-content" id="bg-2">
+<!-- <div class="row home-content" id="bg-2">
 	<div class=" col-xs-12 col-sm-6 col-sm-offset-1">
 		<div id="home-2-content">
-		<?php
-		$page_id = 44;  //Page ID
-		$page_data = get_page( $page_id );
-		global $more;
-		$more = 0;
-		$content = apply_filters('the_content', $page_data->post_excerpt);
-		echo $content;
-		?>
+		// <?php
+		// $page_id = 44;  //Page ID
+		// $page_data = get_page( $page_id );
+		// global $more;
+		// $more = 0;
+		// $content = apply_filters('the_content', $page_data->post_excerpt);
+		// echo $content;
+		// ?>
 		<a href="/about/">
 		<button class="btn btn-read">Full Bio</button>
 		</a>
 		</div>
 	</div>
-</div>
+</div> -->
 <div class="row home-content" id="bg-3">
 	<div class="col-xs-12 col-sm-12 col-md-8 col-lg-8">
 		<div id="home-3-content">

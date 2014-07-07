d78a644c17e3f7f297f78df89edc71c08bb38dd8
michael <michael@12southmusic.com>
2014-07-07 14:46:28 -0500

    Update home page per Dan's Changes

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -1,37 +1,8 @@
 <div class="row" id="bg-1">
 	<div class="col-sm-12 home-slider-container">
+	<div class="name-treatment">
+		<h1>Dan Hochhalter</h1>
+		<h3>Musician / Songwriter</h3>
 	<?php echo do_shortcode('[wpv-view name="home-top-banners"]'); ?>
-	</div>
-</div>
-<!-- <div class="row home-content" id="bg-2">
-	<div class=" col-xs-12 col-sm-6 col-sm-offset-1">
-		<div id="home-2-content">
-		// <?php
-		// $page_id = 44;  //Page ID
-		// $page_data = get_page( $page_id );
-		// global $more;
-		// $more = 0;
-		// $content = apply_filters('the_content', $page_data->post_excerpt);
-		// echo $content;
-		// ?>
-		<a href="/about/">
-		<button class="btn btn-read">Full Bio</button>
-		</a>
-		</div>
-	</div>
-</div> -->
-<div class="row home-content" id="bg-3">
-	<div class="col-xs-12 col-sm-11 col-sm-offset-1 col-md-8 col-lg-8">
-		<div id="home-3-content">
-					<h2>Dan Can<br />
-					Rock Your Track</h2>
-	<?php echo do_shortcode('[wpv-view name="home-audio-slider"]'); ?>
-	<a href="#bg-4"><button class="btn btn-read">Book Dan</button></a> <a href="/sessionwork/"><button class="btn btn-music">More Music</button></a>
-		</div>
-	</div>
-</div>
-<div class="row home-content" id="bg-4">
-	<div class="col-sm-6 col-sm-offset-3">
-		<?php echo do_shortcode('[vfb id=1]'); ?>
 	</div>
 </div>

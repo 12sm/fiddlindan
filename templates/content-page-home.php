b69d20254efeb32e7b0be3248cfdc96a3e8bdddc
michael <michael@12southmusic.com>
2014-08-22 17:06:12 -0500

    Rotating home titles

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -1,9 +1,5 @@
 <div class="row" id="bg-1">
 	<div class="col-sm-12 home-slider-container">
-	<div class="name-treatment">
-		<h1>Dan Hochhalter</h1>
-		<h3>Musician / Songwriter</h3>
-	</div>
 	<?php echo do_shortcode('[wpv-view name="home-top-banners"]'); ?>
 	</div>
 </div>
